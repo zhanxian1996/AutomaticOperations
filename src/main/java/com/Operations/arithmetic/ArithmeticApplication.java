6912bf8c17f9e93c91160d4f7cc69b19f225d5d6
zhanxian1996 <zhanxian1996@gmail.com>
2017-06-13 15:52:11 -0500

    initial commit# Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/Operations/arithmetic/ArithmeticApplication.java
@@ -0,0 +1,12 @@
+package com.Operations.arithmetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmeticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmeticApplication.class, args);
+	}
+}
